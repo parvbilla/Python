29b6b3b256d52a5f6472c3587e00f76d682d9f8f
parvbilla <parvbilla2004@gmail.com>
2026-08-13 13:41:40 +0530

    Variable & Data Types Programs

---
+++ Variables & Data Types/code.py
@@ -0,0 +1,41 @@
+print("hello world")
+print("i name is parv")
+print("my age is 21")
+print(23)
+print(23+34)
+
+name="parv"
+age=34
+price=100
+print(name,age,price)
+print(type(name))
+
+num=10
+num/=5
+num**=2
+print(num)
+
+print(not False)
+print(not True)
+
+a=3.14
+a=str(a)
+print(type(a))
+print(a)
+
+name = input("enter your name ")
+print("welcome ",name)
+
+# practice question
+
+num1=int(input("enter first number :- "))
+num2=int(input("enter second number :- "))
+print(num1+num2)
+
+side =int(input("enter side of a sqaure"))
+print(side*side)
+
+if(num1>=num2):
+    print("True")
+else:
+    print("False")
